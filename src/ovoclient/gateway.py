c10574e299085d46532d3a306b96eea1d208e5dc
martogi.sitanggang <martogi@apps-foundry.com>
2019-04-02 11:40:20 +0700

    refactored ovo to separated charge and reversal also add recursive_reversal based on ovo terms

--- src/ovoclient/gateway.py
+++ src/ovoclient/gateway.py
@@ -15,13 +15,15 @@ from http import HTTPStatus
 import requests
 
 from ovoclient.exceptions import OvoClientError
-from ovoclient.models import PaymentRequest, PaymentResponse
+from ovoclient.models import PaymentRequest, PaymentResponse, ResponseCode
 
 log = logging.getLogger('ovoclient')
 
 __all__ = ['OvoClientGateway', ]
 
-DEFAULT_TIMEOUT = 60
+CHARGE_DEFAULT_TIMEOUT = 65
+REVERSAL_DEFAULT_TIMEOUT = 15
+MAX_REVERSAL_COUNT = 3
 
 
 class OvoClientGateway:
@@ -42,8 +44,8 @@ class OvoClientGateway:
                                   self.secret_key.encode(), hashlib.sha256).digest()
         return base64.b64encode(hmac_signature).decode()
 
-    def payment(self, payment_request: PaymentRequest, timeout: int = None):
-        """Send push to pay transaction
+    def charge(self, payment_request: PaymentRequest, timeout: int = None):
+        """Send push to pay transaction (CHARGE)
 
         :param `ovoclient.models.PaymentRequest payment_request:
         :return:
@@ -62,7 +64,7 @@ class OvoClientGateway:
             response = requests.post(url=url,
                                      data=json.dumps(data),
                                      headers=headers,
-                                     timeout=DEFAULT_TIMEOUT if not timeout else timeout)
+                                     timeout=CHARGE_DEFAULT_TIMEOUT if not timeout else timeout)
             response_json = json.loads(response.content.decode('utf-8')) if response.content else {}
             if response.status_code != HTTPStatus.OK and not response_json:
                 raise OvoClientError("Failed to register into ovo api")
@@ -70,11 +72,60 @@ class OvoClientGateway:
         except (requests.ConnectTimeout, requests.HTTPError, requests.ConnectionError):
             response_json = data
             log.exception(f"Failed to create new ovo payment for order {payment_request.reference_number}")
-        except OvoClientError as exc:
-            raise exc
         transaction_request_data = data.get('transactionRequestData', {})
         response_json['transactionRequestData']['phone'] = transaction_request_data.get('phone', '')
         response_json['transactionRequestData']['merchantInvoice'] = transaction_request_data.get('merchantInvoice',
                                                                                                   '')
-
         return PaymentResponse.from_api_json(response_json)
+
+    def reversal(self, payment_request: PaymentRequest, timeout: int = None, attempt: int = 0):
+        """Send push to pay transaction (REVERSE)
+
+        :param `ovoclient.models.PaymentRequest payment_request:
+        :return:
+        """
+        data = payment_request.serialize()
+        random_number = int(datetime.now().timestamp())
+
+        headers = {
+            'app-id': self.app_id,
+            'Random': f"{random_number}",
+            'Hmac': self.generate_signature(random_number),
+            'Content-Type': 'application/json'
+        }
+        try:
+            url = f'{self.base_url}/pos'
+            attempt += 1
+            response = requests.post(url=url,
+                                     data=json.dumps(data),
+                                     headers=headers,
+                                     timeout=REVERSAL_DEFAULT_TIMEOUT if not timeout else timeout)
+            response_json = json.loads(response.content.decode('utf-8')) if response.content else {}
+            if response.status_code != HTTPStatus.OK and not response_json:
+                raise OvoClientError("Failed to register into ovo api")
+
+        except (requests.ConnectTimeout, requests.HTTPError, requests.ConnectionError, requests.ReadTimeout):
+            response_json = data
+            log.exception(f"Failed to create new ovo reversal for order {payment_request.reference_number}")
+        transaction_request_data = data.get('transactionRequestData', {})
+        response_json['transactionRequestData']['phone'] = transaction_request_data.get('phone', '')
+        response_json['transactionRequestData']['merchantInvoice'] = transaction_request_data.get('merchantInvoice',
+                                                                                                  '')
+        return PaymentResponse.from_api_json(response_json), attempt
+
+    def recursive_reversal(self, payment_request: PaymentRequest, timeout: int = None):
+        """
+        Recursive reversal based on MAX_REVERSAL_COUNT and response from reversal whether its succeeded
+        :param payment_request:
+        :param timeout:
+        :return:
+        """
+        attempt = 0
+        response_json = None
+        while attempt < MAX_REVERSAL_COUNT and (
+                not response_json or
+                response_json.response_code != ResponseCode.SUCCESS.value
+        ):
+            payment_request.date = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            response_json, attempt = self.reversal(payment_request=payment_request, timeout=timeout, attempt=attempt)
+        return response_json
